2d4ecd9d8bb34735f7d65392361aa1a1e7e0f2f7
highfivecode <jon@highfivecode.com>
2017-10-11 10:11:14 -0400

    template tags

--- djangocc/views.py
+++ djangocc/views.py
@@ -5,7 +5,8 @@ def home(request):
     Renders home page
     '''
     greeting = "uStudy - the best study site in the world!"
-    today = 'tuesday'
-    # a dictionary with a keyword 'our_greeting' mapping to the variable greeting defined above.
-    context = {'our_greeting':greeting, 'day_of_week':today} #an empty dictionary
+    #first we will create a variable with the days of the week as a list
+    days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    # then we will add it to the template context
+    context = {'our_greeting':greeting, 'weekday_list':days_of_week}
     return render(request, 'home.html', context)
