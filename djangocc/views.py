a2e662310bf3444f86ff8311d843b95b67ec51f8
highfivecode <jon@highfivecode.com>
2017-10-11 09:04:13 -0400

    video 5

--- djangocc/views.py
+++ djangocc/views.py
@@ -4,5 +4,8 @@ def home(request):
     '''
     Renders home page
     '''
-    context = {} #an empty dictionary
+    greeting = "uStudy - the best study site in the world!"
+    today = 'tuesday'
+    # a dictionary with a keyword 'our_greeting' mapping to the variable greeting defined above.
+    context = {'our_greeting':greeting, 'day_of_week':today} #an empty dictionary
     return render(request, 'home.html', context)
