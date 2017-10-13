492f56adbaeebd78a2063f6199588110b6618632
highfivecode <jon@highfivecode.com>
2017-10-13 17:21:23 -0400

    video 11

--- flashcards/models.py
+++ flashcards/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Deck(models.Model):
     title = models.CharField(max_length=64, null=False, blank=False)
     description = models.CharField(max_length=255, null=False, blank=True)
+
+    def __str__(self):
+        return self.title
