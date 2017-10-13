b7885d31617509797077640485606da3697a9a61
highfivecode <jon@highfivecode.com>
2017-10-13 16:18:21 -0400

    first model

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Deck(models.Model):
+    title = models.CharField(max_length=64, null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=True)
