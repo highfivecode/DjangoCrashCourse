6fb367f27ab4acb5cf472d5d4edeaa3f6a24c8df
highfivecode <jon@highfivecode.com>
2017-10-16 12:12:44 -0400

    video 17 - model admin actinos

--- flashcards/admin.py
+++ flashcards/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Deck, Card
 
+def push_live(modeladmin, request, queryset):
+    rows_updated = queryset.update(is_active=True)
+    if rows_updated == 1:
+        message_bit = "1 deck was"
+    else:
+        message_bit = "%s decks were" % rows_updated
+    modeladmin.message_user(request, "%s successfully marked as active." % message_bit)
+
+push_live.short_description = "Mark selected Decks as active"
+
 class DeckAdmin(admin.ModelAdmin):
-    list_display=('title', 'is_active', 'get_number_of_cards_as_str')
+    list_display=('title', 'is_active', 'get_number_of_cards')
     list_filter=('is_active',)
     search_fields = ['title', 'description']
+    actions = [push_live]
 
 class CardAdmin(admin.ModelAdmin):
     pass
--- flashcards/models.py
+++ flashcards/models.py
@@ -9,10 +9,13 @@ class Deck(models.Model):
     def __str__(self):
         return self.title
 
-    def get_number_of_cards_as_str(self):
-        num = self.card_set.count()
-        return '%s' %(num)
-    get_number_of_cards_as_str.short_description = 'Card Count'
+    def get_number_of_cards(self):
+        '''
+        Returns the number of cards in the decks related
+        card_set
+        '''
+        return self.card_set.count()
+    get_number_of_cards.short_description = 'Card Count'
 
 class Card(models.Model):
     parentDeck = models.ForeignKey(Deck, on_delete=models.CASCADE)
