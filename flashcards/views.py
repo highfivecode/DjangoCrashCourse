316bac7f7a615e04dab3746ef9d6b13272d22479
highfivecode <jon@highfivecode.com>
2017-10-17 08:33:59 -0400

    video 19-forms2

--- flashcards/views.py
+++ flashcards/views.py
@@ -15,8 +15,17 @@ def createDeck(request):
     Renders the form to add new decks to the database
     '''
     if request.method == 'POST':
-        print('****************')
-        print(request.POST)
-        print('****************')
+        title_input = request.POST.get('form_title', None)
+        description_input = request.POST.get('form_description', None)
+        if 'form_is_active' in request.POST:
+            is_active_input = True
+        else:
+            is_active_input = False
+        new_deck = Deck(
+                        title = title_input,
+                        description = description_input,
+                        is_active = is_active_input)
+        new_deck.save()
+        print('************* NEW DECK SAVED ****************')
     context = {}
     return render(request, 'flashcards/createDeck.html', context)
