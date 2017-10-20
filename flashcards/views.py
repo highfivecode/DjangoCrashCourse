d0d8e9f2b35ca82e2d09230c3b10b1fe3ec4208e
highfivecode <jon@highfivecode.com>
2017-10-20 19:31:40 -0400

    video 26 - using url query strings

--- flashcards/views.py
+++ flashcards/views.py
@@ -105,8 +105,7 @@ def viewDeck(request, deck_id):
     deck_obj = get_object_or_404(Deck, id=deck_id)
     card_list = deck_obj.card_set.all()
     card_obj = card_list.first()
-    print(deck_obj)
-    print(card_list)
-    print(card_obj)
+    if request.method == 'GET' and 'card' in request.GET:
+        card_obj = get_object_or_404(Card, id=request.GET['card'])
     context = {'deck_obj': deck_obj, 'card_obj':card_obj}
     return render(request, 'flashcards/viewDeck.html', context)
